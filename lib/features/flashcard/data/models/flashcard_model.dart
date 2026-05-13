9e2de35195791ff55391919301e08c8acf32a4a8
tomzit <barisakib6@gmail.com>
2026-05-13 13:01:21 +0600

    added model

---
+++ lib/features/flashcard/data/models/flashcard_model.dart
@@ -0,0 +1,14 @@
+import 'package:hive/hive.dart';
+
+part 'flashcard_model.g.dart';
+
+@HiveType(typeId: 0)
+class FlashcardModel extends HiveObject {
+  @HiveField(0)
+  final String question;
+
+  @HiveField(1)
+  final String answer;
+
+  FlashcardModel({required this.question, required this.answer});
+}
